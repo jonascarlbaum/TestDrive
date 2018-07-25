0bcdfcdba0881596340672cc49429f7ed21274e9
Jonas Carlbaum <jonas.carlbaum@b3it.se>
2018-07-25 21:43:15 +0200

    Initial SmokeTests commit. Skeleton code.

---
+++ TestDrive.SmokeTests/Features/Sitemap.feature
@@ -0,0 +1,15 @@
+﻿Feature: Sitemap Feature
+	In order to avoid broken URLs
+	As a site visitor
+	I want to be sure all pages of /sitemap.xml returns valid return codes
+
+@smoketest
+Scenario: Can retrieve sitemap
+	Given I visit http://localhost:50874/sitemap.xml
+	Then I should see a valid sitemap
+	
+@smoketest
+Scenario: No URL returns 404
+	Given I visit http://localhost:50874/sitemap.xml
+	And I visit each URL in the response
+	Then I should get no 404 error codes
---
+++ TestDrive.SmokeTests/Features/Steps/SitemapFeatureSteps.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using TechTalk.SpecFlow;
+
+namespace TestDrive.SmokeTests.Features.Steps
+{
+    [Binding]
+    public class SitemapFeatureSteps
+    {
+        [Given(@"I visit http://localhost:(.*)/sitemap\.xml")]
+        public void GivenIVisitHttpLocalhostSitemap_Xml(int p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Given(@"I visit each URL in the response")]
+        public void GivenIVisitEachURLInTheResponse()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"I should see a valid sitemap")]
+        public void ThenIShouldSeeAValidSitemap()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"I should get no (.*) error codes")]
+        public void ThenIShouldGetNoErrorCodes(int p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+    }
+}
