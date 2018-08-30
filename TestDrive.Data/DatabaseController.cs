b734c633eaa255de1947f95a751b06091bf97eec
Jonas Carlbaum <jonas.carlbaum@gmail.com>
2018-08-30 11:31:10 +0200

    Add TestDrive.Data project and related assets.

---
+++ TestDrive.Data/DatabaseController.cs
@@ -0,0 +1,75 @@
+﻿using System.Configuration;
+using System.Data.SqlClient;
+using System.Diagnostics;
+using System.Web.Mvc;
+using EPiServer;
+using EPiServer.ServiceLocation;
+
+namespace TestDrive.Data
+{
+    public class DatabaseController : Controller
+    {
+        public ActionResult RestoreDB()
+        {
+            var resetTimer = Stopwatch.StartNew();
+            ResetEPiDatabase
+               (ConfigurationManager.AppSettings["database:test:database-backup"],
+                ConfigurationManager.AppSettings["database:test:database-name"]);
+            resetTimer.Stop();
+			
+            return Json(
+                new
+                {
+                    ResetEPiDatabaseFromBackupFileIn = resetTimer.ElapsedMilliseconds + " ms"
+                }, JsonRequestBehavior.AllowGet);
+        }
+
+        public ActionResult RestoreContentRich()
+        {
+            var resetTimer = Stopwatch.StartNew();
+            ResetEPiDatabase
+               (ConfigurationManager.AppSettings["database:content-rich:database-backup"],
+                ConfigurationManager.AppSettings["database:content-rich:database-name"]);
+            resetTimer.Stop();
+			
+            return Json(
+                new
+                {
+                    ResetEPiDatabaseFromBackupFileIn = resetTimer.ElapsedMilliseconds + " ms"
+                }, JsonRequestBehavior.AllowGet);
+        }
+
+        private void ResetEPiDatabase(string backup, string databaseName)
+        {
+            RestoreFromBackupFile(backup, databaseName);
+            ServiceLocator.Current.GetInstance<IContentCacheRemover>().Clear();
+        }
+
+        private void RestoreFromBackupFile(string backup, string databaseName)
+        {
+            var connectionString = ConfigurationManager.ConnectionStrings["EPiServerDB"].ConnectionString;
+
+            using (var con = new SqlConnection(connectionString))
+            {
+                con.Open();
+
+                const string useMaster = "USE master";
+                var useMasterCommand = new SqlCommand(useMaster, con);
+                useMasterCommand.ExecuteNonQuery();
+
+                var alter1 = @"ALTER DATABASE [" + databaseName + "] SET Single_User WITH Rollback Immediate";
+                var alter1Cmd = new SqlCommand(alter1, con);
+                alter1Cmd.ExecuteNonQuery();
+
+                var restore = @"RESTORE DATABASE [" + databaseName + "] FROM DISK = N'" + backup +
+                              @"' WITH REPLACE,  NOUNLOAD,  STATS = 10";
+                var restoreCmd = new SqlCommand(restore, con);
+                restoreCmd.ExecuteNonQuery();
+
+                var alter2 = @"ALTER DATABASE [" + databaseName + "] SET Multi_User";
+                var alter2Cmd = new SqlCommand(alter2, con);
+                alter2Cmd.ExecuteNonQuery();
+            }
+        }
+    }
+}
---
+++ TestDrive.Data/Initialization.cs
@@ -0,0 +1,28 @@
+﻿using System.Web.Mvc;
+using System.Web.Routing;
+using EPiServer.Framework;
+using EPiServer.Framework.Initialization;
+
+namespace TestDrive.Data
+{
+    #if DEBUG
+    [ModuleDependency(typeof(EPiServer.Web.InitializationModule))]
+    public class Initialization : IInitializableModule
+    {
+        public void Initialize(InitializationEngine context)
+        {
+            RouteTable.Routes.MapRoute(name: "Database Route",
+                url: "database/{action}",
+                defaults: new { action = "index", controller = "Database" });
+        }
+
+        public void Uninitialize(InitializationEngine context)
+        {
+        }
+
+        public void Preload(string[] parameters)
+        {
+        }
+    }
+    #endif
+}
